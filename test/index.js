7b991e1d658002e09cb8c78bceaef3ffa4a1b68b
fstelluto <fstelluto@pro-vision.de>
2018-03-21 15:32:14 +0100

    Change the way testing works
    - Using tape and stylelint built-in stylelint-test-rule-tape
    - created a new folder test/

---
+++ test/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+/* eslint global-require: "off" */
+
+const ruleTester = require("stylelint-test-rule-tape");
+const stylelintConfigPv = require("..");
+
+// const util = require("util");
+// ["./index.js", "./plain-css.js", "./order.js", "./scss.js"].forEach(
+//   fileName => {
+//     const fileContent = require(fileName);
+//     console.log(util.inspect(fileContent, false, 4).substr(0, 128));
+//     console.log("...");
+//     console.log(`File ${fileName} properly loaded 👍
+//   `);
+//   }
+// );
