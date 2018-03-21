f73a9208ef0616e89a74f4d2aa0685d3b8164a89
fstelluto <fstelluto@pro-vision.de>
2018-03-21 16:35:02 +0100

    Change tests to a simple node script

--- test/index.js
+++ test/index.js
@@ -1,16 +1,65 @@
 /* eslint-disable no-console */
 /* eslint global-require: "off" */
 
-const ruleTester = require("stylelint-test-rule-tape");
-const stylelintConfigPv = require("..");
+const stylelint = require("stylelint");
+const path = require("path");
+const assert = require("assert");
 
-// const util = require("util");
-// ["./index.js", "./plain-css.js", "./order.js", "./scss.js"].forEach(
-//   fileName => {
-//     const fileContent = require(fileName);
-//     console.log(util.inspect(fileContent, false, 4).substr(0, 128));
-//     console.log("...");
-//     console.log(`File ${fileName} properly loaded 👍
-//   `);
-//   }
-// );
+// ////////////////////// test.scss
+
+stylelint
+  .lint({
+    config: require(".."),
+    files: "./test/test.scss",
+    formatter: Function.prototype,
+  })
+  .then(data => {
+    const FILEPATH = "test.scss";
+    const results = extractResults(data);
+    const expected = 0;
+    const actual = results[FILEPATH].length;
+    assert(
+      actual === expected,
+      `${FILEPATH} has ${actual} errors, expected ${expected}`
+    );
+    console.log(`👍  ${FILEPATH}`);
+  })
+  .catch(err => {
+    console.log("Error running tests:", err);
+    process.exit(1);
+  });
+
+// ////////////////////// errors.scss
+
+stylelint
+  .lint({
+    config: require(".."),
+    files: "./test/errors.scss",
+    formatter: Function.prototype,
+  })
+  .then(data => {
+    const FILEPATH = "errors.scss";
+    const results = extractResults(data);
+    const expected = 6;
+    const actual = results[FILEPATH].length;
+    assert(
+      actual === expected,
+      `${FILEPATH} has ${actual} errors, expected ${expected}`
+    );
+    console.log(`👍  ${FILEPATH}`);
+  })
+  .catch(err => {
+    console.log("Error running tests:", err);
+    process.exit(1);
+  });
+
+// ///////////////////////////////////////////////// UTILS
+
+function extractResults(data) {
+  return data.results.reduce((accumulator, current) => {
+    accumulator[path.basename(current.source)] = Array.from(
+      current.warnings
+    );
+    return accumulator;
+  }, {});
+}
